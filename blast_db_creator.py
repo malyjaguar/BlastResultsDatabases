192b6e148a80d1cc09b7a17a464a4c8a2999d4d5
Marjanka <marie.pazoutova@gmail.com>
2023-09-19 14:26:07 +0200

    :tada: Create database layout
    Simple spaghetti code creating MySQL database with four connected tables

--- blast_db_creator.py
+++ blast_db_creator.py
@@ -1,80 +1,28 @@
+#!/usr/bin/env python3
 import sys
 import mysql.connector as connector
-from mysql.connector import errorcode
+# from mysql.connector import errorcode
 
 config = {
   "user": "marie",
-  "password": "***", # Type in your password here
-  # "host": "127.0.0.1"
+  "password": "", # Type in your password here
 }
 
 db_name = "blast_results_fornicata" 
 
-
-def create_connection():
-    conn = None
-    try:
-        conn = connector.connect(**config)
-        cur = conn.cursor()
-        cur.execute("USE {}".format(db_name))
-        return conn
-    except connector.Error as e:
-        if err.errno == errorcode.ER_BAD_HOST_ERROR:
-            print("Chybná adresa DB serveru", err)
-        elif err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print("Zřejmě uživatelské jméno nebo heslo je chybně")
-        elif err.errno == errorcode.ER_BAD_DB_ERROR:
-            print("Databáze neexistuje" , db_name)
-            create_database(conn)
-            print("Databáze vytvořena", db_name)
-            return conn
-        print("Chyba připojení: ", err)
-        exit(1)
-
-
-def create_database(conn):
-    try:
-        cur = conn.cursor()
-        cur.execute("CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(db_name))
-        cur.execute("USE {}".format(db_name))
-    except connector.Error as err:
-        print("Chyba databáze: ", err)
-        exit(1)
-
-
-def main():
-    conn = create_connection()
-    print("Verze DB serveru: ", conn.get_server_info())
-
-
-if __name__ == '__main__':
-    main()
-
-
-
-
-
-##### SUGGESTION #####
-
-conn = mysql.connector.connect(
-    host="localhost",  # Replace with your MySQL server host
-    user="username",   # Replace with your MySQL username
-    password="password" # Replace with your MySQL password
-)
-
-# Create a cursor to execute SQL commands
+conn = connector.connect(**config)
 cursor = conn.cursor()
+cursor.execute(f"DROP DATABASE IF EXISTS {db_name}")
+cursor.execute(f"CREATE DATABASE {db_name} DEFAULT CHARACTER SET 'utf8'")
+cursor.execute(f"USE {db_name}")
+print("DB server version: ", conn.get_server_info())
 
-# Create the database
-database_name = "my_database"
-cursor.execute(f"CREATE DATABASE IF NOT EXISTS {database_name}")
-conn.database = database_name
 
 # Create table 1: organisms
 cursor.execute("""
     CREATE TABLE IF NOT EXISTS organisms (
         id INT AUTO_INCREMENT PRIMARY KEY,
-        species_identifier VARCHAR(255)
+        species_identifier VARCHAR(66)
     )
 """)
 
@@ -83,18 +31,18 @@ cursor.execute("""
     CREATE TABLE IF NOT EXISTS genes (
         id INT AUTO_INCREMENT PRIMARY KEY,
         organism_id INT,
-        gene_identifier VARCHAR(255),
-        orthogroup VARCHAR(255),
+        gene_identifier VARCHAR(36),
         FOREIGN KEY (organism_id) REFERENCES organisms(id)
     )
 """)
 
 # Create table 3: hits
+# TODO: if we are strict, the sseqid VARCHAR lenght can be reduced to 10
 cursor.execute("""
     CREATE TABLE IF NOT EXISTS hits (
         id INT AUTO_INCREMENT PRIMARY KEY,
         gene_id INT,
-        sseqid VARCHAR(255),
+        sseqid VARCHAR(12),
         pident FLOAT,
         length INT,
         matches INT,
@@ -105,7 +53,8 @@ cursor.execute("""
         send INT,
         evalue FLOAT,
         bitscore FLOAT,
-        FOREIGN KEY (gene_id) REFERENCES genes(id)
+        FOREIGN KEY (gene_id) REFERENCES genes(id),
+        INDEX (sseqid)
     )
 """)
 
@@ -113,7 +62,7 @@ cursor.execute("""
 cursor.execute("""
     CREATE TABLE IF NOT EXISTS taxonomy (
         id INT AUTO_INCREMENT PRIMARY KEY,
-        sseqid VARCHAR(255),
+        sseqid VARCHAR(12),
         taxonomy TEXT,
         FOREIGN KEY (sseqid) REFERENCES hits(sseqid)
     )
